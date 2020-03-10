c3f1e8cbc7deed5587c2b468dcfc33844c551ffe
DaniilSvadkovksy <daniil.svadkovsky@gmail.com>
2020-03-10 13:03:06 +0200

    Change the Window class

--- src/ua/simplecode/Forum/Window.java
+++ src/ua/simplecode/Forum/Window.java
@@ -1,4 +1,13 @@
 package ua.simplecode.Forum;
 
-public class Window {
+import javax.swing.*;
+
+public class Window extends JFrame {
+
+    public Window(String name){
+        super(name);
+        setSize(400,400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
 }
